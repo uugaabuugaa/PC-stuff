ad69413eead7526f62d8914f4ea8275203d7c14f
asdf <70348189+uugaabuugaa@users.noreply.github.com>
2024-10-26 20:48:44 +0600

    Update (betterfox)user.js

--- Firefox/Librewolf/(betterfox)user.js
+++ Firefox/Librewolf/(betterfox)user.js
@@ -94,7 +94,7 @@ user_pref("browser.urlbar.quicksuggest.enabled", false);
 user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
 user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
 user_pref("browser.urlbar.groupLabels.enabled", false);
-user_pref("browser.formfill.enable", false);
+user_pref("browser.formfill.enable", true);
 user_pref("security.insecure_connection_text.enabled", true);
 user_pref("security.insecure_connection_text.pbmode.enabled", true);
 user_pref("network.IDN_show_punycode", true);

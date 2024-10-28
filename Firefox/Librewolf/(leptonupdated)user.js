c7e37bccdae9e312fc26d091ba56951e2468747c
asdf <70348189+uugaabuugaa@users.noreply.github.com>
2024-10-28 11:00:36 +0600

    ...

--- Firefox/Librewolf/(leptonupdated)user.js
+++ Firefox/Librewolf/(leptonupdated)user.js
@@ -599,11 +599,11 @@ user_pref("general.smoothScroll.mouseWheel.durationMinMS",      0);            /
 user_pref("general.smoothScroll.mouseWheel.migrationPercent", 100);            ///      100
 user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);   ///      120
 user_pref("general.smoothScroll.msdPhysics.enabled",                  true);   ///    false
-user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 100);   ///     1250
-user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",     100);   ///     1000
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 200);   ///     1250
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",     200);   ///     1000
 user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",         10);   ///       12
 user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",  "1.20");   ///    "1.3"
-user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",   1000);   ///     2000
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",   3000);   ///     2000
 user_pref("general.smoothScroll.other.durationMaxMS",         100);            ///      150
 user_pref("general.smoothScroll.other.durationMinMS",           0);            ///      150
 user_pref("general.smoothScroll.pages.durationMaxMS",         100);            ///      150
@@ -616,7 +616,7 @@ user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6");            /
 user_pref("layers.async-pan-zoom.enabled",                   true);            ///     true
 user_pref("layout.css.scroll-behavior.spring-constant",   "250.0");            ///   "250.0"
 user_pref("mousewheel.acceleration.factor",                     3);            ///       10
-user_pref("mousewheel.acceleration.start",                     -1);            ///       -1
+user_pref("mousewheel.acceleration.start",                     -2);            ///       -1
 user_pref("mousewheel.default.delta_multiplier_x",            100);            ///      100
 user_pref("mousewheel.default.delta_multiplier_y",            100);            ///      100
 user_pref("mousewheel.default.delta_multiplier_z",            100);            ///      100
